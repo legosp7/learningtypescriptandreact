84886e1e3f1e4aedb9121fd232f707d112920fa3
legosp7 <legosp7@gmail.com>
2025-12-12 01:27:59 -0700

    wei!

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,16 +1,55 @@
-import React from 'react';
 import logo from './status.png';
 import Button from '@mui/material/Button'
+import { CssBaseline } from '@mui/material';
+import { useReducer } from 'react';
 import './App.css';
 
+interface MyButtonProps {
+  title: string;
+  disabled: boolean;
+}
 
-function MyButton({ title }: { title: string }) {
-  return <Button variant="contained">{title}</Button>;
+interface State {
+  count: number;
+}
+
+type CounterAction =
+  | { type: 'setCount', value: State["count"] }
+  | { type: 'reset' };
+
+const initialState: State = { count: 0 };
+
+function stateReducer(state: State, action: CounterAction): State {
+  switch (action.type) {
+    case "reset":
+      return initialState;
+    case "setCount":
+      return { ...state, count: action.value };
+    default:
+      throw new Error("Unknown action");
+  }
+}
+
+
+function MyButton({ title, disabled }: MyButtonProps) {
+  return <Button variant="contained" disabled={disabled}>{title}</Button>;
 }
 
 export default function App() {
+
+  const [state, dispatch] = useReducer(stateReducer, initialState);
+  const Wei = () => dispatch({ type: 'setCount', value: state.count + 1 });
+  const reset = () => dispatch({ type: 'reset' });
+  let audio = new Audio('/Audio/wei.mp3')
+
+  const start = () => {
+    audio.play();
+  }
+
+
   return (
     <div className="App">
+      <CssBaseline />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -24,7 +63,15 @@ export default function App() {
         >
           Learn React
         </a>
-        <MyButton title="Oguri as in Peak" />
+        <a><MyButton title="Oguri as in Peak" disabled={false} /></a>
+        <p>Count: {state.count}</p>
+        <div className="buttons" style={{ display: 'flex', gap: '10px' }}>
+          <Button variant="contained" onClick={() => {Wei(); start();}}>Wei!</Button>
+          <Button variant="contained" onClick={() => {reset();}}>Reset</Button>
+        </div>
+        
+
+
       </header>
     </div>
   );
